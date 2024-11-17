4c83f981648af2f9ec76a4b2cd7a6dc2c0c6519d
Dipak Gayakwad <dipaksgaykwad2004@gmail.com>
2024-11-17 22:40:07 +0530

    Create character_to_integer.cpp

---
+++ ImpTechnique/character_to_integer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int digit = 5;           // Input digit
+    char ch = '0' + digit;   // Convert digit to char
+
+    std::cout << "The character representation of digit " << digit << " is: " << ch << std::endl;
+    return 0;
+}
